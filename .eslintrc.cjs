04f8c5526fd93357f15b20387f2b29cbd7e9494e
Michal Svorc <dev@michalsvorc.com>
2023-08-06 12:16:40 +0000

    feat(eslint): add Linting with Type Information
    https://typescript-eslint.io/linting/typed-linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,23 @@
 /* eslint-env node */
 module.exports = {
-  root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.cjs', '*.mjs'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ],
   rules: {
     'no-console': 'warn',
   },
+  root: true,
 };
