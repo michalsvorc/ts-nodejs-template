d07a30cb1257264d5c63e19e625da8fab7f00c1c
Michal Svorc <dev@michalsvorc.com>
2023-08-19 14:26:14 +0000

    chore(scripts): remove obsolete helper scripts

--- scripts/utils/logger.mjs
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-export default ({ stdout, stderr }) =>
-  (stdout || stderr) && console.info(stdout, stderr);
