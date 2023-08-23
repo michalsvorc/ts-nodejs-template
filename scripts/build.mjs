5b55693c0e62e9b59e71055aef896b9824ad7fab
Michal Svorc <dev@michalsvorc.com>
2023-08-23 13:51:35 +0000

    feat(scripts): cleand output directory for builds

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,16 +1,30 @@
+import shell from 'shelljs';
 import esbuild from 'esbuild';
 
-const esbuildOptions = {
-    entryPoints: ['src/index.ts'],
-    format: 'esm',
-    bundle: true,
-    platform: 'node',
-    external: [],
-    outfile: 'dist/index.mjs',
-    logLevel: 'debug',
+const OUTPUT_DIRECTORY = 'dist';
+
+const buildOptions = {
+  entryPoints: ['src/index.ts'],
+  format: 'esm',
+  bundle: true,
+  platform: 'node',
+  external: [],
+  outfile: `${OUTPUT_DIRECTORY}/index.mjs`,
+  logLevel: 'debug',
 };
 
-esbuild.build(esbuildOptions).catch((error) => {
+function clean() {
+  const path = `${OUTPUT_DIRECTORY}/*`;
+  console.info(`Cleaning ${path}`);
+  shell.rm('-rf', path);
+}
+
+function build() {
+  return esbuild.build(buildOptions);
+}
+
+clean();
+build().catch((error) => {
   console.error(error);
   process.exit(1);
 });
